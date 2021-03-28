1ef0de1e8e09c72437253e4f7604b6e8cd8a9092
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-03-28 16:44:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ข้อสอบ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("input number a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("input number b");
+            b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
